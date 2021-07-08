c6146a0662ef5bb11acd666b45179f3d2b629e9c
Amanda McGilchrist <amandamcgilchrist7@gmial.com>
2021-07-08 17:37:49 +1000

    was counting an extra try for when it found the correct number

--- set3/exercise4.py
+++ set3/exercise4.py
@@ -7,52 +7,52 @@ import time
 
 # import time
 
-'''
-def binary_search(low, high, actual_number):'''
+
+def binary_search(low, high, actual_number):
     """Do a binary search.
 
     This is going to be your first 'algorithm' in the usual sense of the word!
     you'll give it a range to guess inside, and then use binary search to home
     in on the actual_number.
-    
+
     Each guess, print what the guess is. Then when you find the number return
     the number of guesses it took to get there and the actual number
     as a dictionary. make sure that it has exactly these keys:
     {"guess": guess, "tries": tries}
-    
+
     This will be quite hard, especially hard if you don't have a good diagram!
-    
-    Use the VS Code debugging tools a lot here. It'll make understanding 
+
+    Use the VS Code debugging tools a lot here. It'll make understanding
     things much easier.
     """
-    '''
+
     tries = 0
     guess = 0
-    guessed = False
-    lowest = low
-    highest = high
+    Found = False
 
-    while not guessed:
-        guessed_number = round((lowest + highest) / 2)
-        print("You guessed {}!".format(guessed_number))
-        if guessed_number == actual_number:
-            guessed = True
-        elif guessed_number > actual_number:
-            highest = guessed_number + 1
-        elif guessed_number < actual_number:
-            lowest = guessed_number + 1
-        tries += 1
-          
-    return {"guess": guess, "tries": tries}
-'''
-'''
+    while high >= low and Found is False:
+        middle = (low + high) // 2
+        print(f"You guessed {middle}!")
+        if actual_number < middle:
+            high = middle - 1
+            tries += 1
+        elif actual_number == middle:
+            Found = True
+        else:
+            low = middle + 1
+            tries += 1
+    if Found is True:
+        print("Found")
+    else:
+        print("Not Found")
+
+    return {"guess": middle, "tries": tries}
+
+
 if __name__ == "__main__":
-    print(binary_search(1, 100, 6))
-'''
-'''
+
     print(binary_search(1, 100, 5))
     print(binary_search(1, 100, 6))
     print(binary_search(1, 100, 95))
     print(binary_search(1, 51, 5))
     print(binary_search(1, 50, 5))
-'''
